2cb48098110b16ccbbe170813d95fb686d8701fc
aduki2912 <s1260241@gmail.com>
2019-07-05 11:34:36 +0900

    Initial revision

---
+++ DiceGame.py
@@ -0,0 +1,8 @@
+import random
+print("Rolling the dice・・・")
+all = 0
+for i in range(2):
+    num = random.randint(1,6)
+    print("Die {}: {} ".format(i + 1,num))
+    all += int(num)
+print("Total value: {}".format(all))
